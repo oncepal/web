0fbf98baf5d8fabe64faa2c15fda488dd8d6c3f0
Sylas Xu <xymzhs@gmail.com>
2024-09-23 22:21:08 +0800

    feat: 更新app

--- apps/native/app/(aux)/index.tsx
+++ apps/native/app/(aux)/index.tsx
@@ -4,36 +4,27 @@ import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
-import { Link } from 'expo-router';
-
+import { Link, router } from 'expo-router';
 export default function HomeScreen() {
   return (
-   
-      <Link href="/(aux)" asChild>
+
+      <ThemedView style={styles.titleContainer}>
+        <ThemedText type="title">Welcome!</ThemedText>
+        <HelloWave />
+        <Link href="/(tabs)" asChild>
       <Pressable>
         <ThemedText>Home</ThemedText>
       </Pressable>
     </Link>
-
+      </ThemedView>
+    
   );
 }
 
 const styles = StyleSheet.create({
   titleContainer: {
-    height:200,
     flexDirection: 'row',
     alignItems: 'center',
     gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
+  }
 });
--- apps/native/components/ParallaxScrollView.tsx
+++ apps/native/components/ParallaxScrollView.tsx
@@ -12,7 +12,7 @@ import { ThemedView } from '@/components/ThemedView';
 const HEADER_HEIGHT = 250;
 
 type Props = PropsWithChildren<{
-  headerImage: ReactElement;
+  headerImage?: ReactElement;
   headerBackgroundColor: { dark: string; light: string };
 }>;
 
@@ -64,10 +64,12 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   header: {
+    padding:200,
     height: 250,
     overflow: 'hidden',
   },
   content: {
+    marginTop:200,
     flex: 1,
     padding: 32,
     gap: 16,
